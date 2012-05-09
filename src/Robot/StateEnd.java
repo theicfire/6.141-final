d851b52cd2f7772c7af45c5ef4865f69ef0dcf87
theicfire@gmail.com <theicfire@gmail.com>
2012-05-09 16:00:44 -0400

    stuff

---
+++ src/Robot/StateEnd.java
@@ -0,0 +1,20 @@
+package Robot;
+
+import java.awt.geom.Point2D;
+
+public class StateEnd extends RobotState {
+
+	public StateEnd(Robot ri) {
+		super(ri);
+	}
+
+	@Override
+	public void perform() {
+		// robot.arm.raiseArm();
+		// robot.armDriver.doMovement(robot.arm);
+		// this.robot.setStateObject(new StateLookingForBlocks(this.robot));
+		while (true) {
+			// victory dance
+		}
+	}
+}
---
+++ src/Robot/StateMakingStructure.java
@@ -0,0 +1,42 @@
+package Robot;
+
+import Controller.Utility;
+
+public class StateMakingStructure extends RobotState {
+	
+	public StateMakingStructure(Robot ri) {
+		super(ri);
+	}
+
+	enum State {
+		INIT
+	}
+
+	@Override
+	public void perform() {
+		State state = State.INIT;
+		boolean done = false;
+		while (!done) {
+			switch (state) {
+				case INIT: {
+					robot.log.info("entry ENTRY ENTRY making struct");
+////					this.robot.stopMoving();
+////					this.robot.driveForward(0);					
+////					Utility.sleepFor250ms();
+//					Utility.sleepFor250ms();
+//					this.robot.stopMoving();
+					Utility.sleepFor5Seconds();
+					this.robot.doorDriver.openDoor();
+					Utility.sleepFor5Seconds();
+					this.robot.driveForward(0);
+					Utility.sleepFor5Seconds();
+					this.robot.stopMoving();
+					// open the door
+					done = true;
+					robot.setStateObject(new StateEnd(robot));
+					break;
+				}
+			} // end switch
+		} // end while
+	}
+}
